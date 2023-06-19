3e437f8d2cfc4951d291d4753a14212db72341f4
Lucute2001 <lch.ryu2001@gmail.com>
2023-06-19 23:25:29 +0700

    2. Design SignUp Page

--- app/src/main/java/com/example/foodorderapp/SplashActivity.java
+++ app/src/main/java/com/example/foodorderapp/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                startActivity(new Intent(SplashActivity.this, CreateAccountActivity.class));
                 finish();
             }
         }, 1000);
